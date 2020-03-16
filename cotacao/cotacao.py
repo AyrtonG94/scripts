07210b3aec527bb4e2e27281b4cc5e212eb1c75e
AyrtonG94 <ayrton.gomes@live.com>
2020-03-16 20:14:19 -0300

    ARQUIVOS INICIAIS, NENHUMA ALTERACAO NO MOMENTO

---
+++ cotacao/cotacao.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+listar = open('listagem.txt')
+print(listar.read())
+listar.close()
+
+pergunta = (input('Digite a cotação desejada: \n'))
+
+requisao = requests.get('https://economia.awesomeapi.com.br/json/all/'+str(pergunta))
+
+resposta = json.loads(requisao.text)
+converter = dict(resposta)
+
+for resultado in converter.values():
+    print('Converter:', resultado['code'])
+    print('Para: ', resultado['codein'])
+    print('Valor atual: ', "R$"+resultado['bid'])
+    print('Maior valor do dia até agora: ', "R$"+(resultado['high']))
+    print('Menor valor do dia até agora: ', "R$"+resultado['low'])
