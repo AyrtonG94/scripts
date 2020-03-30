7195d8a09377770b4faa1dda2696cac7265f985e
Ayrton Gomes <60196988+AyrtonG94@users.noreply.github.com>
2020-03-30 16:03:48 -0300

    atualização
    Adicionado um tratamento de erros

--- CEP.py
+++ CEP.py
@@ -1,14 +1,21 @@
 import requests
 import json
 
-pergunta = input('Digite o seu cep sem os traços: \n')
+try:
+   pergunta = int(input('Digite o seu cep sem os traços: \n'))
 
-requisao = requests.get(' https://cep.awesomeapi.com.br/json/'+str(pergunta))
+   requisao = requests.get(' https://cep.awesomeapi.com.br/json/'+str(pergunta))
 
-endereco = json.loads(requisao.text)
+   endereco = json.loads(requisao.text)
 
-print('Rua : ', endereco['address'])
-print('Bairro: ', endereco['district'])
-print('Cidade: ', endereco['city'])
-print('Estado: ', endereco['state'])
-print('CEP: ', endereco['cep'])
+   print('Rua : ', endereco['address'])
+   print('Bairro: ', endereco['district'])
+   print('Cidade: ', endereco['city'])
+   print('Estado: ', endereco['state'])
+   print('CEP: ', endereco['cep'])
+
+except ValueError:
+    print('Digite apenas numeros sem traços, pontos ou letras')
+
+
+
